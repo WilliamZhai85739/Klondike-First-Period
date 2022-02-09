81779d362f1421700e6b787058a456cbc2aa28bd
WilliamZhai85739 <99352316+WilliamZhai85739@users.noreply.github.com>
2022-02-09 09:17:15 -0800

    Update Card.java

--- src/Card.java
+++ src/Card.java
@@ -1,5 +1,14 @@
 
 /** represents a playing card that can draw itself. */
 public class Card implements Drawable, Updateable{
-
+  private int Value;
+  private int Suit;
+  private boolean Showing;
+  private boolean Selected;
+  public Card(int v, int s) {
+    Value = v;
+    Suit = s;
+    Showing = false;
+    Selected = false;
+  }
 }
