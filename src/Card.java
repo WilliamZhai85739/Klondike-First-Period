52c858dfa57cb85c7f18c531ef1541ad888ca530
WilliamZhai85739 <99352316+WilliamZhai85739@users.noreply.github.com>
2022-02-09 09:46:20 -0800

    Update Card.java

--- src/Card.java
+++ src/Card.java
@@ -1,41 +1,48 @@
 
 /** represents a playing card that can draw itself. */
+/** represents a playing card that can draw itself. */
 public class Card implements Drawable, Updateable{
-  private int Value;
-  private int Suit;
-  private boolean Showing;
-  private boolean Selected;
-  public 
-  public Card(int v, int s) {
-    Value = v;
-    Suit = s;
-    Showing = false;
-    Selected = false;
-  }
-  public int getValue() {
-    return Value;
-  }
-  public int getSuit() {
-    return Suit;
-  }
-  public boolean getShowing() {
-    return Showing;
-  }
-  public boolean getSelected() {
-    return Selected;
-  }
-  public void Select() {
-    Selected = true;
-  }
-  public void Deselect() {
-    Selected = false;
-  }
-  public boolean compareTo(Card c) {
-    if(c.getValue() == Value + 1) {
-      return true;
+    private int Value;
+    private int Suit;
+    private boolean Showing;
+    private boolean Selected;
+    public 
+    public void draw(Graphics g) {
+
     }
-    else {
-      return false;
+    public void update(ActionEvent a) {
+        
+    }
+    public Card(int v, int s) {
+      Value = v;
+      Suit = s;
+      Showing = false;
+      Selected = false;
+    }
+    public int getValue() {
+      return Value;
+    }
+    public int getSuit() {
+      return Suit;
+    }
+    public boolean getShowing() {
+      return Showing;
+    }
+    public boolean getSelected() {
+      return Selected;
+    }
+    public void Select() {
+      Selected = true;
+    }
+    public void Deselect() {
+      Selected = false;
+    }
+    public boolean compareTo(Card c) {
+      if(c.getValue() == Value + 1) {
+        return true;
+      }
+      else {
+        return false;
+      }
     }
   }
-}
