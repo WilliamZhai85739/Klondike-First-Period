28fccd545ad1f12bb26b045aa5e2a6b4d2c66bbe
WilliamZhai85739 <99352316+WilliamZhai85739@users.noreply.github.com>
2022-02-16 08:58:45 -0800

    Update Card.java

--- src/Card.java
+++ src/Card.java
@@ -1,14 +1,32 @@
 
 /** represents a playing card that can draw itself. */
 /** represents a playing card that can draw itself. */
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.awt.Graphics;
+import javax.imageio.ImageIO;
+import java.awt.event.*;
+
+
+
+
 public class Card implements Drawable, Updateable{
     private int Value;
     private int Suit;
     private boolean Showing;
     private boolean Selected;
-    public 
+    private Image Spritesheet;
+    private Image drawThis;
+    private int width = 71;
+    private int height = 96;
     public void draw(Graphics g) {
-
+      
+    }
+    public void draw(Graphics g, int x, int y) {
+      drawThis = ((BufferedImage)Spritesheet).getSubimage((Value-1)*width,(Suit-1)*height,width,height);
+      g.drawImage(drawThis, x, y, null);
     }
     public void update(ActionEvent a) {
         
@@ -18,6 +36,14 @@ public class Card implements Drawable, Updateable{
       Suit = s;
       Showing = false;
       Selected = false;
+      if(Spritesheet == null) {
+        try {
+          File f = new File("../images/AllCards.png");
+          Spritesheet = ImageIO.read(f);
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
     }
     public int getValue() {
       return Value;
