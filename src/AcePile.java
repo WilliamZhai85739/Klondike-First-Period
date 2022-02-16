884fbeb2f88f7993e2a970e19de7fcc410dbff7e
zephanjt <99279931+zephanjt@users.noreply.github.com>
2022-02-16 09:27:23 -0800

    Update AcePile.java

--- src/AcePile.java
+++ src/AcePile.java
@@ -1,3 +1,5 @@
 public class AcePile extends Pile {
   public boolean canAddCard(card c) {
-    if(c.getValue() = pile.get(1).getValue() 
+    if(c.getValue() = pile.get(1).getValue(){
+      AcePile.set(0,c);
+    } 
