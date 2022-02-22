7c1d28fd7f01a30890836121c1bdbe6e5ebd7195
WilliamZhai85739 <99352316+WilliamZhai85739@users.noreply.github.com>
2022-02-22 08:52:27 -0800

    Update AcePile.java

--- src/AcePile.java
+++ src/AcePile.java
@@ -12,15 +12,20 @@ public class AcePile extends Pile {
     super();
   }
   public void draw(Graphics g) {
-
+    
+  }
+  public void draw(Graphics g, int x, int y) {
+    if(pile.size() > 0) {
+      pile.get(1).draw(g, x, y);
+    }
   }
   public void update(ActionEvent a) {
-    
+
   }
   public boolean canAddCard(Card c) {
     if(c.getSuit() == pile.get(1).getSuit()) {
       if(c.getValue() == pile.get(1).getValue()){
-        pile.set(0,c);
+        pile.add(0, c);
         return true;
       } 
     }
