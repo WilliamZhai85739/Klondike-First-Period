482296327c8df78869e20efaa9b78decad818e39
WilliamZhai85739 <99352316+WilliamZhai85739@users.noreply.github.com>
2022-02-28 08:57:15 -0800

    Update DrawPile.java

--- src/DrawPile.java
+++ src/DrawPile.java
@@ -1,1 +1,33 @@
+import java.awt.*;
+import java.util.*;
+import java.awt.event.*;
+import java.awt.Graphics;
+
+
+
+
+
+
+
+
+public class DrawPile extends Pile {
+    public DrawPile() {
+        super();
+    }
+    public void draw(Graphics g) {
+        
+    }
+    public void draw(Graphics g, int x, int y) {
+        if(pile.size() > 0) {
+            pile.get(0).draw(g, x, y);
+        }
+    }
+    public void update(ActionEvent a) {
+    
+    }
+    public boolean canAddCard(Card c) {
+        return false;
+    }
+    
+}
 
